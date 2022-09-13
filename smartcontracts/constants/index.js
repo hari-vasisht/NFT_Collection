b73df814b00408448d1fce484da6ba93825bd2df
hari-vasisht <shsvasisht@gmail.com>
2022-09-13 15:32:29 +0530

    fixed bug in index.js

--- smartcontracts/constants/index.js
+++ smartcontracts/constants/index.js
@@ -1,2 +1,4 @@
 const WHITELIST_CONTRACT_ADDRESS = "0x47BC948aDbF04dEA113c968Cf5715Ca17170c541";
 const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
